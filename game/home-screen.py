2237c32c58857b8cadb323ffea0382863c08109f
coder489 <aai34@georgetown.edu>
2020-04-28 19:02:20 -0700

    Clean up code in home-screen file

--- game/home-screen.py
+++ game/home-screen.py
@@ -13,21 +13,12 @@ pygame.display.set_caption('Champions are Coming')
 
 display_screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-img_dir = path.join(path.dirname(__file__), 'images')
-
-WHITE = (225,225,225)
 
 #Code help to understand structure of the start screen from https://github.com/joshuawillman/The-Lonely-Shooter
-def draw_text(surface, text, size, x, y, color):
 
-    font = pygame.font.Font(pygame.font.match_font('cambria'), size)
-    text_surface = font.render(text, True, color)
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (x, y)
-    surface.blit(text_surface, text_rect)
 
 def start_screen():
-    
+    img_dir = path.join(path.dirname(__file__), 'images')
     title = pygame.image.load(path.join(img_dir, "title_text.png")).convert_alpha()
     title = pygame.transform.scale(title, (WIDTH, 165))
     background = pygame.image.load('game\images\Home_Screen.jpg').convert()
@@ -35,10 +26,19 @@ def start_screen():
 
     arrow_keys = pygame.image.load(path.join(img_dir, 'arrow_keys.png')).convert_alpha()
     arrow_keys = pygame.transform.scale(arrow_keys, (150, 85))
-    
+
     display_screen.blit(background, background_rect)
     display_screen.blit(title, (0,110))
     display_screen.blit(arrow_keys, (720, 570))
+
+    def draw_text(surface, text, size, x, y, color):
+
+        font = pygame.font.Font(pygame.font.match_font('cambria'), size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        surface.blit(text_surface, text_rect)
+
     draw_text(display_screen, "Are You Ready for the Challenge?", 35, WIDTH/2, HEIGHT/2, WHITE)
     draw_text(display_screen, "If so, press [ENTER] to begin", 35, WIDTH/2, (HEIGHT/2) + 50, WHITE)
     draw_text(display_screen, "If not, press [Q] to quit", 35, WIDTH/2, (HEIGHT/2) + 100, WHITE)
